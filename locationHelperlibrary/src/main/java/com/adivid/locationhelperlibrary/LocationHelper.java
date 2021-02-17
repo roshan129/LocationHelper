870f170460452f433471907e4e1e12f159b6386b
roshan129 <roshanadke2015>
2021-02-17 13:54:20 +0530

    Initial commit

---
+++ locationHelperlibrary/src/main/java/com/adivid/locationhelperlibrary/LocationHelper.java
@@ -0,0 +1,9 @@
+package com.adivid.locationhelperlibrary;
+
+public class LocationHelper {
+
+    public static String getCustomFormattedString(String string){
+        return string + "-"+ string;
+    }
+
+}
